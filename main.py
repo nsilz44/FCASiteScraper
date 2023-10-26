a5bca8a1a9d506700a57f537273ba514e93a2d7f
Nsilz44 <n_stbss@hotmail.com>
2023-10-26 17:37:51 +0100

    fixed some bugs

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+import bs4
 import pandas as pd
-
+def has_p_but_no_li(tag):
+    return tag.has_attr('p') and not tag.has_attr('ol')
 for i in range(1,8):
     SPECIFIC_PAGE = "https://www.handbook.fca.org.uk/handbook/PROD/" +str(i)+"/?view=chapter"
+    #SPECIFIC_PAGE = "https://www.handbook.fca.org.uk/handbook/PROD/1/?view=chapter"
 
     MAIN_URL = "https://www.fca.org.uk/"
 
@@ -25,35 +28,38 @@ for i in range(1,8):
     soup = BeautifulSoup(page.content, "html.parser")
 
     results = soup.find(class_="full-chapter")
+    sections = results.find("section")
+    if not sections:
+        continue
     sections = results.find_all("section")
     for section in sections:
-        try:
-            details = section.find(class_ = "details")
-            rule_reference = details.find(class_ = "extended").text
-            rule = details.find(class_ = "section-type").text
-            times = details.find("time").text
-            print(rule_reference)
-            print(rule)
-            print(times.strip())
-
-            paragraph = section.find(class_ = ["rule","UK","evidential","guidance"])
-            subsection = paragraph.find("ol")
-            if not subsection:
-                    el = paragraph.find_all('p')
-                    for ellsections in el:
-                        print(ellsections.text.strip())
-                        df.loc[len(df)] = [rule_reference,rule,times.strip(),"",ellsections.text.strip()]
-            if subsection:
-                allSubsections = subsection.find_all('li', recursive=False)
-                for subsections in allSubsections:
-                    el = subsections.find_all('p')
-                    for ellsections in el:
-                        sub = ellsections.previous_sibling.text
-                        print(sub,ellsections.text)
-                        ['Rule reference','Rule or Guidance','Date','Sub sections','Full text']
-                        df.loc[len(df)] = [rule_reference,rule,times.strip(),sub,ellsections.text]
-        except:
+        details = section.find(class_ = "details")
+        if not details:
             continue
+        rule_reference = details.find(class_ = "extended").text
+        rule = details.find(class_ = "section-type").text
+        times = details.find("time").text
+        print(rule_reference)
+        print(rule)
+        print(times.strip())
+        paragraph = section.find(class_ = ["rule","UK","evidential","guidance"])
+        children = paragraph.findChildren(recursive=False)
+        subsection = paragraph.find("ol")
+        if not subsection:
+                el = paragraph.find_all('p')
+                for ellsections in el:
+                    print(ellsections.text.strip())
+                    df.loc[len(df)] = [rule_reference,rule,times.strip(),"",ellsections.text.strip()]
+        if subsection:
+            external_ps = children[1].find_all(('p','ol'))
+            for ps in external_ps:
+                a = ps.find('li', recursive=False)
+                if not a:
+                    if not ps.previous_sibling:
+                        continue
+                    print(ps.previous_sibling.text.strip(),ps.text.strip())
+                    ['Rule reference','Rule or Guidance','Date','Sub sections','Full text']
+                    df.loc[len(df)] = [rule_reference,rule,times.strip(),ps.previous_sibling.text.strip(),ps.text.strip()]
     print(df.head(10))
     df.to_excel("output" + str(i)+".xlsx")
 
